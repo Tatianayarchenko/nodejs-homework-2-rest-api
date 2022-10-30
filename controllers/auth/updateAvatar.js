a347901a330b3d493b02f0832b58f9cf30ee8fcd
Tatianayarchenko <ktanya61@gmail.com>
2022-10-30 18:54:36 +0200

    done

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises');
 const path = require('path');
+const Jimp = require('jimp');
 
 const { User } = require('../../models/user');
 
@@ -7,11 +8,23 @@ const avatarDir = path.join(__dirname, '../../', 'public', 'avatars');
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
+
   const { path: tempUpload, originalname } = req.file;
+
   const extention = originalname.split('.').pop();
+
   const filename = `${_id}.${extention}`;
+
   const resultUpload = path.join(avatarDir, filename);
+
   await fs.rename(tempUpload, resultUpload);
+
+  Jimp.read(resultUpload)
+    .then((avatar) => {
+      return avatar.resize(250, 250).write(resultUpload);
+    })
+    .catch((err) => console.log(err));
+
   const avatarURL = path.join('avatars', filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({
