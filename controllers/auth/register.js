40b75f9563507bd31ce85ebee70746b7bd772fe9
Tatianayarchenko <ktanya61@gmail.com>
2022-11-10 19:11:36 +0200

    done

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,9 +1,12 @@
 const bcrypt = require('bcryptjs');
 const gravatar = require('gravatar');
+const { nanoid } = require('nanoid');
 
 const { User } = require('../../models/user');
 
-const { RequestError } = require('../../helpers');
+const { RequestError, sendEmail } = require('../../helpers');
+
+const { BASE_URL } = process.env;
 
 const register = async (req, res) => {
   const { email, subscription, password } = req.body;
@@ -13,12 +16,23 @@ const register = async (req, res) => {
   }
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
   const result = await User.create({
     email,
     subscription,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
+
+  const mail = {
+    to: email,
+    subject: 'Verify email',
+    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click to verify your email</a>`,
+  };
+
+  await sendEmail(mail);
+
   res.status(201).json({
     email: result.email,
     subscription: result.subscription,
